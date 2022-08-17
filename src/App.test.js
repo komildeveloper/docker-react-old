9937fdd0fcc49a0e0916fb68b64a7a127693376d
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-08-18 01:29:19 +0500

    updated feature branch src/App.js file

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,12 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  const linkElement = screen.getByText(/I was changed on the Feature branch/i)
   expect(linkElement).toBeInTheDocument()
 })
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  const linkElement = screen.getByText(/I was changed on the Feature branch/i)
   expect(linkElement).toBeInTheDocument()
 })
